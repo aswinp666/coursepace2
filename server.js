2e09f5a2965dbe6cfee5a2994486770f8efc4970
aswinp666 <aswinpash666@gmail.com>
2025-04-09 14:27:22 +0530

    server port changes

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ app.post('/api/scores', async (req, res) => {
 
 //  Start Server
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
