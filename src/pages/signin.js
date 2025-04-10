f75c6a3fcd88c88d1005a18d62851244106142f6
aswinp666 <aswinpash666@gmail.com>
2025-04-10 21:11:34 +0530

    signinchanges

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -14,7 +14,7 @@ const SignIn = () => {
     setLoading(true)
 
     try {
-      const res = await fetch('http://localhost:5000/signin', {
+      const res = await fetch('/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
